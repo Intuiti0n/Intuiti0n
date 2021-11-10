04de39f432b07cca77bfb626a265855425d5b9e7
Intuiti0n <danielgomescames@gmail.com>
2021-11-10 23:18:22 +0000

    still working on squid game project

--- SquidGame_ThreeJS/src/js/main.js
+++ SquidGame_ThreeJS/src/js/main.js
@@ -5,12 +5,20 @@ const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
-renderer.setClearColor(0xb7c3f3,1);//background color,opacity
+renderer.setClearColor(0xb7c3f3, 1);//background color,opacity
 
-const light = new THREE.AmbientLight( 0xffffff ); // soft white light
-scene.add( light );
+const light = new THREE.AmbientLight(0xffffff); // soft white light
+scene.add(light);
 
 console.log("Good work");
+
+function createCube(size,positionX) {
+    const geometry = new THREE.BoxGeometry(size.w,size.h,size.d);
+    const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+    const cube = new THREE.Mesh(geometry, material);
+    cube.position.x = positionX;
+    scene.add(cube);
+}
 
 camera.position.z = 5;
 
@@ -19,34 +27,61 @@ const loader = new THREE.GLTFLoader();
 
 
 class Doll {
-    constructor(){
+    constructor() {
         // Load a glTF resource
-loader.load("./model/scene.gltf",
-// called when the resource is loaded, callback function
-function ( gltf ) {
+        loader.load("./model/scene.gltf", (gltf) => {
 
-    scene.add( gltf.scene );
-    gltf.scene.scale.set(.4,.4,.4);
-    gltf.scene.position.set(0,-1,0);
-}
-);
+            scene.add(gltf.scene);
+            gltf.scene.scale.set(.4, .4, .4);
+            gltf.scene.position.set(0, -1, 0);
+            this.doll = gltf.scene;
+        });
+    }
+
+    lookBackward() {
+        //this.doll.rotation.y = Math.PI;
+        gsap.to(this.doll.rotation, { y: -3.15, duration: .45 })
+    }
+
+    lookForward() {
+        //this.doll.rotation.y = 0;
+        gsap.to(this.doll.rotation, { y: 0, duration: .45 })
     }
 }
 
+function createTrack() {
+    createCube({w:.2,h:.5,d:.5},4);
+}
+
+createTrack();
 
 let doll = new Doll;
-function animate () {
-    requestAnimationFrame(animate);    
+
+setTimeout(() => {
+    doll.lookBackward();
+    console.log("Look back");
+}, 1000);
+
+
+setTimeout(() => {
+    doll.lookForward();
+    console.log("Look forward");
+}, 2000);
+
+
+//doll.lookBack();
+function animate() {
+    requestAnimationFrame(animate);
     renderer.render(scene, camera);
 };
 
 animate();
 
-window.addEventListener('resize',onWindowResize,false);
+window.addEventListener('resize', onWindowResize, false);
 
-function onWindowResize(){
+function onWindowResize() {
     camera.aspect = window.innerWidth / window.innerHeight;
     camera.updateProjectionMatrix();
 
-    renderer.setSize(window.innerWidth,window.innerHeight);
+    renderer.setSize(window.innerWidth, window.innerHeight);
 }
